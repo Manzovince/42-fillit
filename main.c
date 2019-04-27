4f212d598a1ddd2e554c5542099c01649b3d9d17
Manzovince <manzovince@outlook.fr>
2019-04-27 16:43:43 +0200

    main.c print_error modified

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vmanzoni <vmanzoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 13:20:48 by vmanzoni          #+#    #+#             */
-/*   Updated: 2019/04/27 15:14:37 by vmanzoni         ###   ########.fr       */
+/*   Updated: 2019/04/27 16:36:09 by vmanzoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int		main(int argc, char **argv)
 	if (argc == 2)
 	{
 		if (!(input = read_file(argv[1])))
-			print_error("Error: Could not read file.\n");
+			print_error("error: Could not read file.\n");
 		if (check_file_errors(input))
-			print_error("Error: Invalid file.\n");
+			print_error("error: Invalid file.\n");
 		parse_input(input);
 		/*
 		Backtracking for smallest square
@@ -29,6 +29,6 @@ int		main(int argc, char **argv)
 		*/
 	}
 	else
-		print_error("Usage: Please submit a file.\n>>> ./fillit file.fillit\n");
+		print_error("usage: Please submit a file.\n> ./fillit file.fillit\n");
 	return (0);
 }
