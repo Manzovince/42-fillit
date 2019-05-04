6558b77ef2634f9b62ba6c11feea85959e561fd2
Manzovince <manzovince@outlook.fr>
2019-05-04 19:16:38 +0200

    Edit print_color for every tetri working

--- print_map_with_colors.c
+++ print_map_with_colors.c
@@ -6,7 +6,7 @@
 /*   By: vmanzoni <vmanzoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 20:27:22 by vmanzoni          #+#    #+#             */
-/*   Updated: 2019/05/04 19:08:25 by vmanzoni         ###   ########.fr       */
+/*   Updated: 2019/05/04 19:15:35 by vmanzoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,17 +61,17 @@ void ft_putchar_cyan(char c)
 
 void	ft_put_tetri_color(char c)
 {
-	if (c == 'A' || c == 'I' || c == 'O' || c == 'U')
+	if (c == 'A' || c == 'G' || c == 'M' || c == 'S')
 		ft_putchar_red(c);
-	else if (c == 'B' || c == 'J' || c == 'P' || c == 'V')
+	else if (c == 'B' || c == 'H' || c == 'N' || c == 'T' || c == 'Y')
 		ft_putchar_blue(c);
-	else if (c == 'C' || c == 'K' || c == 'Q' || c == 'W')
+	else if (c == 'C' || c == 'I' || c == 'O' || c == 'U' || c == 'Z')
 		ft_putchar_green(c);
-	else if (c == 'D' || c == 'L' || c == 'R' || c == 'X')
+	else if (c == 'D' || c == 'J' || c == 'P' || c == 'V')
 		ft_putchar_yellow(c);
-	else if (c == 'E' || c == 'M' || c == 'S' || c == 'Y')
+	else if (c == 'E' || c == 'K' || c == 'Q' || c == 'W')
 		ft_putchar_magenta(c);
-	else if (c == 'F' || c == 'N' || c == 'T' || c == 'Z')
+	else if (c == 'F' || c == 'L' || c == 'R' || c == 'X')
 		ft_putchar_cyan(c);
 	else
 		ft_putchar(c);
